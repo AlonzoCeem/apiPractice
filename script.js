be7d0f4ac32613992e18ca6ba9fc28afd264e67a
Alonzo <alonzo@Alonzo>
2023-08-29 20:06:08 -0600

    Fixed formatting

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ async function render(){
     await getChars();
     const names = charactersInfo.map((obj) => {
         let hashName = getHashName(obj.name);
-        return `<div class="name"><a href="${hashName}">${obj.name}</a></diV>`;
+        return `
+        <div class="name">
+            <a href="${hashName}">${obj.name}</a>
+        </diV>`
     }).join("");
     charContainer.innerHTML = names;
     console.log(charactersInfo)
@@ -35,13 +38,13 @@ function displayCharInfo(hash){
             const html = `
             <h1>${obj.name}</h1>
             <div>
-            <h2>Gender: ${obj.gender}</h2>
-            <h2>Birth Year: ${obj.birth_year}</h2>
-            <h2>Eye Color: ${obj.eye_color}</h2>
-            <h2>Hair Color: ${obj.hair_color}</h2>
-            <h2>Skin color: ${obj.skin_color}</h2>
-            <h2>Height: ${obj.height}</h2>
-            <h2>Mass: ${obj.mass}</h2>
+                <h2>Gender: ${obj.gender}</h2>
+                <h2>Birth Year: ${obj.birth_year}</h2>
+                <h2>Eye Color: ${obj.eye_color}</h2>
+                <h2>Hair Color: ${obj.hair_color}</h2>
+                <h2>Skin color: ${obj.skin_color}</h2>
+                <h2>Height: ${obj.height}</h2>
+                <h2>Mass: ${obj.mass}</h2>
             </div>
             `
             selectedContainer.innerHTML = html;
